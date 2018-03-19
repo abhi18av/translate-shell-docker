62818ee9bcc787f3dee6a6d5f6dbd94c2d8189a9
Abhinav Sharma <abhi18av@gmail.com>
2018-03-19 18:36:15 +0530

    updatedthe local trans shell

--- build.ps1
+++ build.ps1
@@ -1,22 +1,31 @@
-NOTE: I'm using the single file install from https://github.com/soimort/translate-shell
+#NOTE: I'm using the single file install from https://github.com/soimort/translate-shell
+
+
+sudo docker rmi abhi18av/translate-shell-docker --force                                                                    
+
+
+wget git.io/trans -O /bin/trans
 
 sudo docker build -t translate-shell-docker .
 
 sudo docker run -it abhi18av/translate-shell-docker trans --version      
 
 
-sudo docker push abhi18av/translate-shell-docker                                                                           
 sudo docker run -it abhi18av/translate-shell-docker trans en:es "hello" -engine bing   
 
 
-sudo docker rmi abhi18av/translate-shell-docker --force                                                                    
-docker pull abhi18av/translate-shell-docker                                                                                
-sudo docker pull abhi18av/translate-shell-docker                                                                           
-sudo docker run -it abhi18av/translate-shell-docker trans --version   
-
-
-
-docker build -t translate
 docker tag translate abhi18av/translate-shell-docker                                                                                                                 
-docker push abhi18av/translate-shell-docker                                                                                  
+
+sudo docker push abhi18av/translate-shell-docker                                                                           
+
+
+#docker pull abhi18av/translate-shell-docker                                                                                
+#sudo docker pull abhi18av/translate-shell-docker                                                                           
+#sudo docker run -it abhi18av/translate-shell-docker trans --version   
+
+
+
+#docker build -t translate
+#docker tag translate abhi18av/translate-shell-docker                                                                                                                 
+#docker push abhi18av/translate-shell-docker                                                                                  
 
